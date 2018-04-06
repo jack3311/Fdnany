bfbf332e8e06f2df5c723925d0daed539034295f
Jack Wilson <jack.wilson3311@gmail.com>
2018-04-06 17:13:16 +1200

    something

--- JEngine/JEngineLib/ComponentManager.cpp
+++ JEngine/JEngineLib/ComponentManager.cpp
@@ -8,6 +8,21 @@ namespace JEngine
 {
 	namespace ECS
 	{
+		void ComponentManager::destroyComponentInternal(int _entity, size_t _componentTypeHash)
+		{
+			//Find list of components (by entity id) of this type
+			auto & currComponent = components[typeid(T).hash_code()];
+
+			//Find component memory
+			T * allocated = reinterpret_cast<T*>(currComponent[_entity]);
+
+			//Clear address in map
+			currComponent[_entity] = nullptr;
+
+			//Return allocated memory to pool
+			componentAllocator.deallocate(allocated);
+		}
+
 		ComponentManager::ComponentManager()
 		{
 
